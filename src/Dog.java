fd85960ee21b608fbe490c57447f3981533dcfa3
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-10-20 13:00:15 -0400

    Second Commit

--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,4 @@
-public class Dog extends Animal{
+public class Dog extends Animal{ // Animal is the super class of the subclass Dog.
 
     public Dog() {
         super("St. Bernard", "Giant", 145);
